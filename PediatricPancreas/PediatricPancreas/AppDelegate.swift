483ed9bba3c49717c7878b1bd213b549e0f862b6
jeffcheng1234 <36403947+jeffcheng1234@users.noreply.github.com>
2020-02-17 06:00:38 -0800

    Installed CocoaPods and connected to Firebase
    All developers who are pulling this version will have to download cocoapods and install the two firebase toolkits we need : Coredata and database. The app is configured to our firebase.

--- PediatricPancreas/PediatricPancreas/AppDelegate.swift
+++ PediatricPancreas/PediatricPancreas/AppDelegate.swift
@@ -11,7 +11,7 @@ import ResearchKit
 import CareKitUI
 import CareKitStore
 import CareKit
-
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
---
+++ PediatricPancreas/Pods/Target Support Files/Pods-PediatricPancreas/Pods-PediatricPancreas-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PediatricPancreas : NSObject
+@end
+@implementation PodsDummy_Pods_PediatricPancreas
+@end
