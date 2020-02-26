cfadc3d3b3c18065b5956911586cadc51070b9ee
Taewon Kim <taewon@berkeley.edu>
2020-02-26 00:59:48 -0800

    Resource Display

---
+++ PediatricPancreas/PediatricPancreas/Resource.swift
@@ -0,0 +1,20 @@
+//
+//  Resource.swift
+//  resources
+//
+//  Created by Taewon Kim on 2/23/20.
+//  Copyright © 2020 Taewon Kim. All rights reserved.
+//
+
+import Foundation
+
+class Resource {
+    
+    var labelName: String
+//    var pointsTo: Resource
+    
+    init(labelName: String) {
+        self.labelName = labelName
+//        self.pointsTo = pointsTo
+    }
+}
---
+++ PediatricPancreas/PediatricPancreas/ResourceCell.swift
@@ -0,0 +1,19 @@
+//
+//  ResourceCell.swift
+//  resources
+//
+//  Created by Taewon Kim on 2/23/20.
+//  Copyright © 2020 Taewon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ResourceCell: UITableViewCell {
+
+    @IBOutlet weak var resourceName: UILabel!
+    
+    func setResource(resource: Resource) {
+        resourceName.text = resource.labelName
+    }
+    
+}
